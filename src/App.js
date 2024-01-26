fc4ad9482f2ed626e8ec0de8ccf0eaefbe943dab
hanyn1 <hanine.azizi@esprit.tn>
2024-01-26 10:24:04 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import Rgister from './components/Rgister';
+import AboutUs from './components/AboutUs';
+import Femme from './components/Femme';
+import Homme from './components/Homme';
+import Pannier from './components/Pannier';
+import HomeClient from './components/HomeClient';
+import { CartProvider } from './components/CartContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>HK STORE</title>
+        <link rel='icon' href='/logo.png' type="image/x-icon" />
+      </Helmet>
+      <CartProvider>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/Register' element={<Rgister />} />
+            <Route path='/about' element={<AboutUs />} />
+            <Route path='/Homme' element={<Homme />} />
+            <Route path='/Femme' element={<Femme />} />
+            <Route path='/Pannier' element={<Pannier />} />
+            {/* Wrap HomeClient with CartProvider */}
+            <Route path='/Client' element={<HomeClient />} />
+          </Routes>
+        </Router>
+      </div>
+      </CartProvider>
+    </HelmetProvider>
   );
 }
 
